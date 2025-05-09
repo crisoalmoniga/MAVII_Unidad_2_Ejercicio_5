96e65d956b2835e86f919cffcf2ec2e13c2057bd
Criso <68233846+crisoalmoniga@users.noreply.github.com>
2025-05-08 23:31:18 -0300

    inicio repo

--- EsqueletoAPP/Box2dFirstApp/Src/Box2dEmptyApp.cpp
+++ EsqueletoAPP/Box2dFirstApp/Src/Box2dEmptyApp.cpp
@@ -3,12 +3,8 @@
 
 using namespace sf;
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-    // Crear el objeto de la clase Game
-    Game* Juego;
-    Juego = new Game(800, 600, "Esqueleto de Aplicación - MAVII");
-    Juego->Loop(); // Ejecutar el bucle principal del juego
-
-    return 0; // Retorna 0 indicando que el programa se ha ejecutado correctamente
+int main() {
+    Game game;
+    game.run();  // En lugar de game.Loop();
+    return 0;
 }
--- EsqueletoAPP/Box2dFirstApp/Src/Game.h
+++ EsqueletoAPP/Box2dFirstApp/Src/Game.h
@@ -1,44 +1,26 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include "SFMLRenderer.h"
-#include <list>
-#include <Box2D/Box2D.h>
+#include "D:/Universidad - Cris/Videojuegos/Universidad del Litoral/MAV2/2025/MAVII2025_Trabajo_practico_final/MAVII2025_Trabajo_practico_final/EsqueletoAPP/Box2dFirstApp/Build/Header Files/Level.h"
+#include "D:/Universidad - Cris/Videojuegos/Universidad del Litoral/MAV2/2025/MAVII2025_Trabajo_practico_final/MAVII2025_Trabajo_practico_final/EsqueletoAPP/Box2dFirstApp/Build/Header Files/Menu.h"
+#include "D:/Universidad - Cris/Videojuegos/Universidad del Litoral/MAV2/2025/MAVII2025_Trabajo_practico_final/MAVII2025_Trabajo_practico_final/EsqueletoAPP/Box2dFirstApp/Build/Header Files/Counter.h"
 
-using namespace sf;
-class Game
-{
-private:
-	// Propiedades de la ventana
-	int alto;
-	int ancho;
-	RenderWindow *wnd;
-	Color clearColor;
 
-	// Objetos de box2d
-	b2World *phyWorld;
-	SFMLRenderer *debugRender;
 
-	//tiempo de frame
-	float frameTime;
-	int fps;
-
-	// Cuerpo de box2d
-	b2Body* controlBody;
-
+class Game {
 public:
+    Game();
+    void run();
 
-	// Constructores, destructores e inicializadores
-	Game(int ancho, int alto,std::string titulo);
-	void CheckCollitions();
-	void CreateEnemy(int x, int y);
-	~Game(void);
-	void InitPhysics();
+private:
+    void processEvents();
+    void update();
+    void render();
 
-	// Main game loop
-	void Loop();
-	void DrawGame();
-	void UpdatePhysics();
-	void DoEvents();
-	void SetZoom();
+    sf::RenderWindow window;
+    Level* currentLevel;
+    Menu* menu;
+    Counter* counter;
+
+    enum GameState { MENU, PLAYING, PAUSED, GAME_OVER };
+    GameState state;
 };
